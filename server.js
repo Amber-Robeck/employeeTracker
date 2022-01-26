d284cf745f52a83db8ddd1380b1db3a63e302b6b
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-01-25 21:54:45 -0600

    created a switch statement in init function for each choice in prompt choices

--- server.js
+++ server.js
@@ -18,11 +18,44 @@ const init = () => {
             type: "list",
             name: "name",
             message: "What would you like to do today?",
-            choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
+            choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role", "I'm finsished"]
         },
     ])
         .then(choices => {
             console.log(choices.name);
+            switch (choices.name) {
+                case "View all departments":
+                    console.log("this will be view departments")
+                    break;
+
+                case "View all roles":
+                    console.log("this will be view roles");
+                    break;
+                case "View all employees":
+                    console.log("this will be view employees");
+                    break;
+
+                case "Add a department":
+                    console.log("this will be add department");
+                    break;
+
+                case "Add a role":
+                    console.log("this will be add a role");
+                    break;
+
+                case "Add an employee":
+                    console.log("this will be add an employee");
+                    break;
+
+                case "Update an employee role":
+                    console.log("this will be update employee role");
+                    break;
+
+                case "I'm finsished":
+                    console.log("all done");
+                    break;
+
+            }
         });
 }
 init();
